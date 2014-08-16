f1ae0ccfe09aeae2fd6cccfbc9c22e8a40b054c2
jasonleibowitz <jasonaleibowitz@gmail.com>
2014-08-16 19:02:04 -0400

    update chart.js

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,3 +1,32 @@
 $( document ).ready(function() {
     console.log( "loaded bro!" );
+
+    var data = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+        {
+            label: "My First dataset",
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: [65, 59, 80, 81, 56, 55, 40]
+        }
+      ]
+  };
+
+    var options = {scaleShowGridLines: true};
+    var ctx = $("#myChart").get(0).getContext("2d");
+    var myNewChart = new Chart(ctx);
+    var myLineChart = new Chart(ctx).Line(data, options);
 });
+
+function getLabels(){
+
+}
+
+function getData(){
+
+}
